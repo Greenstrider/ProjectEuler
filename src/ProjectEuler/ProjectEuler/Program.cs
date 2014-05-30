664cb2620b688a0339915b649501de503e5431b3
sp1ze <anis.alekic@gmail.com>
2014-05-30 17:00:08 +0200

    #1

--- src/ProjectEuler/ProjectEuler/Program.cs
+++ src/ProjectEuler/ProjectEuler/Program.cs
@@ -8,9 +8,20 @@ namespace ProjectEuler
 {
     class Program
     {
+        private static ProblemSolver ps = new ProblemSolver();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What problem would you like to solve?");
+
+            int problemNumber;
+            string cmd = Console.ReadLine();
+            bool isInt = int.TryParse(cmd, out problemNumber);
+
+            if (isInt)
+                ps.SolveProblem(problemNumber);
+            else
+                Console.WriteLine("Noob that is no number!");
+
             Console.Read();
         }
     }
